f2ec9b42f65c4dd5151ec78db18b55a14fb75d12
Josef Vrbata <jvrbata@komtesa.com>
2016-08-09 09:46:20 +0200

    path update

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,10 +5,10 @@ use Symla\Joomla\Environment\Environment;
 use Symla\Joomla\Environment\Loader;
 
 
-require_once(__DIR__ . '/../../../../libs/autoload.php');
+// require_once('path/to/autoloader');
 
 $loader      = new Loader();
-$environment = new Environment($loader->load(__DIR__ . '/../../../.env'));
+$environment = new Environment($loader->load('path/to/.env'));
 
 if ($environment->environment() !== $environment::PRODUCTION) {
     Symla\Joomla\Tracy\Debugger::enable();
